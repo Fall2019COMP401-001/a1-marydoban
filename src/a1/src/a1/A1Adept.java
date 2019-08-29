f72cd93c90595b848e2eb8e25b0dd7d167c568aa
marydoba <marydoba@marydoba-pc.wireless-1x.unc.edu>
2019-08-29 18:36:15 -0400

    added a space

--- src/a1/src/a1/A1Adept.java
+++ src/a1/src/a1/A1Adept.java
@@ -1,14 +1,30 @@
 package a1;
 
 import java.util.Scanner;
+import.java.util.HashMap
 
 public class A1Adept {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
 		// Your code follows here.
+		String[] output = new String[3];
+	int selection = scan.nextInt();
+	Hashmap<String, Double> menu = new HashMap<String, Double>; 
+	for (int i = 0; i < selection; i++) {
+		menu.put(scan.next(), scan.nextDouble());	
+	}
+	int customers = scan.nextInt();
+	String[] Customers = new String[customers];
+	Double[] spent = new Double[customers];
+	for (int i = 0; i < customers; i++) {
+		Customers[i] = scan.next(); + " " + scan.next();
+		itemsbought = scan.nextInt();
+		for (int j = 0; j < itemsbought; i++) {
+			spent[i] = ( scan.nextInt(); * menu.get(scan.next()));
+		}
 		
 	}
+	System.out.prinln("Biggest: " +  )
 }
