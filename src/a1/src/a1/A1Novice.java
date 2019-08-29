e6147e7da26f1147f3f4af1dd1d6ef00efa5cc44
marydoba <marydoba@marydoba-pc.wireless-1x.unc.edu>
2019-08-29 18:33:43 -0400

    trying again

---
+++ src/a1/src/a1/A1Novice.java
@@ -0,0 +1,37 @@
+package a1;
+
+import java.util.Scanner;
+
+public class A1Novice {
+
+	public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int count = scan.nextInt();
+
+        String[] output = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            String first = scan.next();
+            String last = scan.next();
+            int items = scan.nextInt();
+
+            double total = 0;
+
+            for (int j = 0; j < items; j++) {
+                double quant = scan.nextDouble();
+                scan.next();
+                double price = scan.nextDouble();
+
+                total += quant * price;
+            }
+
+            output[i] = first.charAt(0) + ". " + last + String.format("%.2f", total);
+
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(output[i]);
+        }
+    }
+}
