e4ddefba0d61c3b290dce3bfa1871190e501f656
marydoba <marydoba@marydoba-pc.wireless-1x.unc.edu>
2019-08-30 12:08:22 -0400

    just wanna check novice, commented out code

--- src/a1/src/a1/A1Adept.java
+++ src/a1/src/a1/A1Adept.java
@@ -1,30 +1,52 @@
 package a1;
 
 import java.util.Scanner;
-import.java.util.HashMap
+import java.util.HashMap;
 
 public class A1Adept {
 
-	public static void main(String[] args) {
+	/* public static void main(String[] args) {
+		
+		static double findValueMax(Double[] vals) {
+			
+			// Initialize current minimum to first value in array.
+			double cur_max = vals[0];
+			
+			// Starting with second value (if any), compare each value
+			// in array with current minimum and replace if smaller.
+			
+			for (int i=1; i < vals.length; i++) {
+				if (vals[i] > cur_max) {
+					cur_max = vals[i];
+				}
+			}
+			
+			return cur_max;
+		}
 		
 		Scanner scan = new Scanner(System.in);
 		// Your code follows here.
-		String[] output = new String[3];
-	int selection = scan.nextInt();
-	Hashmap<String, Double> menu = new HashMap<String, Double>; 
-	for (int i = 0; i < selection; i++) {
-		menu.put(scan.next(), scan.nextDouble());	
+	
+		int selection = scan.nextInt();
+		
+		HashMap<String, Double> menu = new HashMap<String, Double>; 
+		
+		for (int i = 0; i < selection; i++) {
+			menu.put(scan.next(), scan.nextDouble());	
 	}
-	int customers = scan.nextInt();
-	String[] Customers = new String[customers];
-	Double[] spent = new Double[customers];
-	for (int i = 0; i < customers; i++) {
-		Customers[i] = scan.next(); + " " + scan.next();
-		itemsbought = scan.nextInt();
-		for (int j = 0; j < itemsbought; i++) {
-			spent[i] = ( scan.nextInt(); * menu.get(scan.next()));
+		int customers = scan.nextInt();
+		String[] Customers = new String[customers];
+		Double[] spent = new Double[customers];
+		for (int k = 0; k < customers; k++) {
+			Customers[i] = scan.next() + " " + scan.next();
+			int itemsbought = scan.nextInt();
+			for (int j = 0; j < itemsbought; j++) {
+				spent[i] = ( scan.nextInt() * menu.get(scan.next()));
 		}
+	}
 		
 	}
-	System.out.prinln("Biggest: " +  )
+	System.out.println("Biggest: " + findValueMax(spent) + )
+	}
 }
+*/
