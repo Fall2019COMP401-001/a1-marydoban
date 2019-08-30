774dd652f475ff2c71d3af506befbb2b5784ed93
marydoba <marydoba@marydoba-pc.wireless-1x.unc.edu>
2019-08-30 12:42:48 -0400

    restarted adept

--- src/a1/src/a1/A1Adept.java
+++ src/a1/src/a1/A1Adept.java
@@ -1,52 +1,32 @@
 package a1;
 
 import java.util.Scanner;
-import java.util.HashMap;
+
 
 public class A1Adept {
 
-	/* public static void main(String[] args) {
-		
-		static double findValueMax(Double[] vals) {
-			
-			// Initialize current minimum to first value in array.
-			double cur_max = vals[0];
-			
-			// Starting with second value (if any), compare each value
-			// in array with current minimum and replace if smaller.
-			
-			for (int i=1; i < vals.length; i++) {
-				if (vals[i] > cur_max) {
-					cur_max = vals[i];
-				}
-			}
-			
-			return cur_max;
-		}
+	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		// Your code follows here.
+		
+		int amntOfItems = scan.nextInt();
+		
+		String[] items = new String[amntOfItems];
+		
+		double[] itemPrices = new double[amntOfItems];
 	
-		int selection = scan.nextInt();
-		
-		HashMap<String, Double> menu = new HashMap<String, Double>; 
-		
-		for (int i = 0; i < selection; i++) {
-			menu.put(scan.next(), scan.nextDouble());	
-	}
-		int customers = scan.nextInt();
-		String[] Customers = new String[customers];
-		Double[] spent = new Double[customers];
-		for (int k = 0; k < customers; k++) {
-			Customers[i] = scan.next() + " " + scan.next();
-			int itemsbought = scan.nextInt();
-			for (int j = 0; j < itemsbought; j++) {
-				spent[i] = ( scan.nextInt() * menu.get(scan.next()));
-		}
-	}
-		
-	}
-	System.out.println("Biggest: " + findValueMax(spent) + )
+		for (int i = 0; i < amntOfItems; i++) {
+			items[i] = scan.next();
+			itemPrices[i] = scan.nextDouble();
+			
+			}
+			
+
+		for (int i = 0; i < amntOfItems; i++) {
+            System.out.println(items[i]);
+            System.out.println(itemPrices[i]);
 	}
 }
-*/
+}
+		
+		
